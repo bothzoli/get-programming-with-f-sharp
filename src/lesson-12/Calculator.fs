9c269a01ac999269d48e9f3aa0c74bb5199a5b56
Zoltan Both <wd973464@varian.com>
2020-09-28 07:53:47 +0200

    Lesson 12

---
+++ src/lesson-12/Calculator.fs
@@ -0,0 +1,10 @@
+module Calculator
+
+[<AutoOpen>]
+module Basic =
+    let add a b = a + b
+    let subtract a b = a - b
+
+module Extra =
+    let multiply a b = a * b
+    let divide a b = a / b
---
+++ src/lesson-12/Program.fs
@@ -0,0 +1,18 @@
+﻿open Domain
+open Operations
+open Calculator
+
+[<EntryPoint>]
+let main argv =
+    let joe = {
+        FirstName = "Joe"
+        LastName = "Blogs"
+        Age = 21
+    }
+
+    if joe |> isOlderThan 18 then printfn "%s is an adult" joe.FirstName
+    else printfn "%s is a child" joe.FirstName
+
+    printfn "%d + %d = %d" 1 2 (add 1 2)
+    printfn "%d * %d = %d" 1 2 (Extra.multiply 1 2)
+    0 // return an integer exit code
