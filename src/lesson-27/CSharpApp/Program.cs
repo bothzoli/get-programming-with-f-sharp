9a93d9ac5f6ce396a2c058ce8e37d8b1bcb119ee
bothzoli <bothzoli@gmail.com>
2020-11-14 14:41:58 +0100

    Lesson 27

---
+++ src/lesson-27/CSharpApp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+using Model;
+
+namespace CSharpApp
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var car = new Car(4, "Super-Car", Tuple.Create(1.5, 3.5));
+            Console.WriteLine(car);
+            Console.WriteLine(car.GetType()); // Model.Car
+            Console.WriteLine($"Car dimensions are {car.Dimensions.Item1}x{car.Dimensions.Item2}");
+
+            var bike = Vehicle.NewMotorbike("Harley-Davidson", 3.5);
+            Console.WriteLine(bike);
+            Console.WriteLine(bike.GetType()); // Model.Vehicle+Motorcar
+            Console.WriteLine($"Bike is a motorcar = {bike.IsMotorcar}");
+            Console.WriteLine($"Bike is a motorbike = {bike.IsMotorbike}");
+
+            var fCar = Functions.CreateCar(4, "FunctionCar", 1.5, 3.5);
+            Console.WriteLine(fCar);
+            Console.WriteLine(fCar.GetType()); // Model.Car
+
+            var fourWheeledCar = Functions.CreateFourWheeledCar
+                .Invoke("4-Wheeled Car")
+                .Invoke(1.5)
+                .Invoke(3.5);
+            Console.WriteLine(fourWheeledCar);
+            Console.WriteLine(fourWheeledCar.GetType()); // Model.Car
+
+            var anotherFourWheeledCar = Functions.Partial("Another 4-Wheeled Car", 1.5, 3.5);
+            Console.WriteLine(anotherFourWheeledCar);
+            Console.WriteLine(anotherFourWheeledCar.GetType()); // Model.Car
+
+            var cliMutableCar = new CliMutableCar();
+            Console.WriteLine(cliMutableCar);
+            cliMutableCar.Brand = "NewBrand";
+            Console.WriteLine(cliMutableCar);
+            cliMutableCar.Wheels = 5;
+            Console.WriteLine(cliMutableCar);
+        }
+    }
+}
---
+++ src/lesson-27/FSharpCode/Library.fs
@@ -0,0 +1,25 @@
+﻿namespace Model
+
+type Car =
+    { Wheels: int
+      Brand: string
+      Dimensions: float * float }
+
+[<CLIMutable>]
+type CliMutableCar =
+    { Wheels: int
+      Brand: string }
+
+type Vehicle =
+    | Motorcar of Car
+    | Motorbike of Name: string * EngineSize: float
+
+module Functions =
+    let CreateCar wheels brand x y =
+        { Wheels = wheels
+          Brand = brand
+          Dimensions = x,y}
+
+    let CreateFourWheeledCar = CreateCar 4
+
+    let Partial brand x y = CreateCar 4 brand x y
